fea738e9cbc57efee856adb83bc8837ff748a05c
EstefaMH <estefymoncaleano@gmail.com>
2024-05-24 22:10:51 -0500

    fix header styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,24 +5,42 @@ import menu from '../../utils/img/bx-menu.svg'
 
 function Navbar() {
     return (
-        <nav className='navbar container-name'>
+        <nav className="navbar container-name">
             <div>
-                <Link to="/" className="logo"><h1 className='name'>Em</h1></Link>
+                <Link to="/" className="logo">
+                    <h1 className="name">Em</h1>
+                </Link>
             </div>
 
             <input type="checkbox" id="toggler"></input>
-            <label for="toggler"><img src={menu} /></label>
+            <label for="toggler">
+                <img alt="hamburguer-menu" src={menu} />
+            </label>
 
-            <div className=' menu container-navbar'>
-
-                <ul className='list'>
-                    <Link to="/" className='item'>Home</Link>
-                    <Link to="/aboutme" className='item'>Perfil</Link>
-                    <Link to="/skills" className='item'>Habilidades</Link>
-                    <Link to="/studies" className='item'>Estudios</Link>
+            <div className=" menu container-navbar">
+                <ul className="list">
+                    <li>
+                        <Link to="/" className="item">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/aboutme" className="item">
+                            Perfil
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/skills" className="item">
+                            Habilidades
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/studies" className="item">
+                            Estudios
+                        </Link>
+                    </li>
                 </ul>
             </div>
-
         </nav>
     );
 }
